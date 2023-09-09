dc863e0bc9b905aa6000a3e58eb8ede22454189c
Daniel Herrero Hernando <danielherrerohernando@gmail.com>
2023-09-09 12:07:26 +0200

    feat: serving app for every route

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import { WebSocketServer } from 'ws'
 import express from 'express'
 
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const wsServer = new WebSocketServer({ noServer: true });
 
 app.use(express.static('frontend/build'))
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+});
 
 const users = new Set();
 
